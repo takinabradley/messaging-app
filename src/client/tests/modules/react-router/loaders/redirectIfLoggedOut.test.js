6dffdafb274c04ee53b70e4e79640c633e23516c
Bradley Finney <77649883+takinabradley@users.noreply.github.com>
2025-01-26 17:55:13 -0500

    add redirectIfLoggedOut test

---
+++ src/client/tests/modules/react-router/loaders/redirectIfLoggedOut.test.js
@@ -0,0 +1,53 @@
+import { afterAll, beforeAll, beforeEach, describe } from "vitest"
+import redirectIfLoggedOut from "../../../../modules/react-router/loaders/redirectIfLoggedOut"
+import auth from "../../../../modules/auth"
+
+const isLoggedIn = vi.fn(async () => true)
+
+vi.mock("../../../../modules/auth", async (importOriginal) => {
+  const original = await importOriginal()
+  return {
+    default: {
+      hi: "there",
+      get isLoggedIn() {
+        return isLoggedIn()
+      }
+    }
+  }
+})
+
+beforeEach(() => {
+  isLoggedIn.mockReset()
+})
+
+describe("redirectIfLoggedOut", () => {
+  test("returns null if auth.isLoggedIn returns true", async () => {
+    isLoggedIn.mockImplementationOnce(async () => true)
+    const requestObj = { url: "http://www.hello.com" }
+    console.log(await redirectIfLoggedOut({ request: requestObj }))
+  })
+
+  test("returns a redirect response to login page if auth.isLoggedIn returns false", async () => {
+    isLoggedIn.mockImplementationOnce(async () => false)
+    const requestObj = { url: "http://www.hello.com/" }
+    const response = new Response(null, {
+      status: 302,
+      statusText: "",
+      headers: { location: "/login?redirectTo=/" }
+    })
+
+    expect(await redirectIfLoggedOut({ request: requestObj })).toEqual(response)
+  })
+
+  test("returns a redirect response to login page with proper redirectTo param if auth.isLoggedIn returns false", async () => {
+    isLoggedIn.mockImplementationOnce(async () => false)
+    const requestObj = { url: "http://www.hello.com/somewhere" }
+    const response = new Response(null, {
+      status: 302,
+      statusText: "",
+      headers: { location: "/login?redirectTo=/somewhere" }
+    })
+
+    expect(await redirectIfLoggedOut({ request: requestObj })).toEqual(response)
+  })
+})
